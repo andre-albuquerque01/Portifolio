8282cb28686bf62265d7e6556975ef0bce7f6028
André Albuquerque <97198432+andre-albuquerque01@users.noreply.github.com>
2022-12-12 16:47:55 -0300

    Inicio
    Adicionando o projeto inteiro

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".logo").click(function () {
+        window.location.href = "index.html"
+    })
+    $(".home").click(function () {
+        window.location.href = "index.html"
+    })
+    $(".estudo1").click(function () {
+        window.location.href = "estudo1.html"
+    })
+    $(".estudo2").click(function () {
+        window.location.href = "estudo2.html"
+    })
+    $(".estudo3").click(function () {
+        window.location.href = "estudo3.html"
+    })
+    $(".about").click(function () {
+        window.location.href = "about.html"
+    })
+})
